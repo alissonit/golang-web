61d295695e06108abec1f5bf210d719ce1379b97
Alisson Castro <alissoncastroskt@gmail.com>
2023-06-23 23:54:31 -0300

    removing dirty code

--- main.go
+++ main.go
@@ -3,17 +3,7 @@ package main
 import (
 	"main/routes"
 	"net/http"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type Product struct {
-	Id          primitive.ObjectID
-	Name        string
-	Description string
-	Price       float64
-	Quantity    int
-}
 
 func main() {
 	routes.LoadRoutes()
